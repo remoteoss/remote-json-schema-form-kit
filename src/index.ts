e190372e4a4b7e03727ce0cc2608386aeb7564cd
Gabriel Garcia <gabriel.garcia@remote.com>
2025-11-25 15:16:30 +0100

    add form errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {
   createHeadlessForm as createHeadlessFormNext,
   type CreateHeadlessFormOptions,
+  type FormErrors as FormErrorsNext,
   modify as modifyNext,
   type ValidationResult as ValidationResultNext,
 } from '@remoteoss/json-schema-form'
@@ -14,6 +15,7 @@ type FormSchemaNext = Parameters<typeof createHeadlessFormNext>[0]
 type FormSchemaLegacy = Parameters<typeof createHeadlessFormV0>[0]
 type FormSchema = FormSchemaNext | FormSchemaLegacy
 type ValidationResultLegacy = ReturnType<typeof createHeadlessFormV0>['handleValidation']
+type FormErrorsLegacy = ReturnType<ReturnType<typeof createHeadlessFormV0>['handleValidation']>['formErrors']
 
 interface JsfOptions extends CreateHeadlessFormOptions {
   nextVersion?: boolean
@@ -25,6 +27,7 @@ export type FormResult = ReturnType<typeof createHeadlessFormNext> | ReturnType<
 export type ValidationResult = ValidationResultNext | ValidationResultLegacy
 export type ModifyResult = ReturnType<typeof modifyNext> | ReturnType<typeof modifyV0>
 export type ModifyConfig = Parameters<typeof modifyNext>[1] | Parameters<typeof modifyV0>[1]
+export type FormErrors = FormErrorsNext | FormErrorsLegacy
 
 function isNextVersion(formSchema: FormSchema, { nextVersion }: { nextVersion?: boolean, [key: string]: any } = { nextVersion: false }) {
   return formSchema?.['x-rmt-meta']?.jsfVersion === '1' || nextVersion
