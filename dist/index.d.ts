fae43d42f54e29fae66d7dce7e0b3abf13760ce3
Gabriel Garcia <gabriel.garcia@remote.com>
2025-11-25 15:16:49 +0100

    update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,11 @@
-import { createHeadlessForm as createHeadlessForm$1, ValidationResult as ValidationResult$1, modify as modify$1, CreateHeadlessFormOptions } from '@remoteoss/json-schema-form';
+import { createHeadlessForm as createHeadlessForm$1, ValidationResult as ValidationResult$1, modify as modify$1, FormErrors as FormErrors$1, CreateHeadlessFormOptions } from '@remoteoss/json-schema-form';
 import { createHeadlessForm as createHeadlessForm$2, modify as modify$2 } from '@remoteoss/json-schema-form-v0-deprecated';
 
 type FormSchemaNext = Parameters<typeof createHeadlessForm$1>[0];
 type FormSchemaLegacy = Parameters<typeof createHeadlessForm$2>[0];
 type FormSchema = FormSchemaNext | FormSchemaLegacy;
 type ValidationResultLegacy = ReturnType<typeof createHeadlessForm$2>['handleValidation'];
+type FormErrorsLegacy = ReturnType<ReturnType<typeof createHeadlessForm$2>['handleValidation']>['formErrors'];
 interface JsfOptions extends CreateHeadlessFormOptions {
     nextVersion?: boolean;
     [key: string]: any;
@@ -13,7 +14,8 @@ type FormResult = ReturnType<typeof createHeadlessForm$1> | ReturnType<typeof cr
 type ValidationResult = ValidationResult$1 | ValidationResultLegacy;
 type ModifyResult = ReturnType<typeof modify$1> | ReturnType<typeof modify$2>;
 type ModifyConfig = Parameters<typeof modify$1>[1] | Parameters<typeof modify$2>[1];
+type FormErrors = FormErrors$1 | FormErrorsLegacy;
 declare function createHeadlessForm(formSchema: FormSchema, jsfOptions?: JsfOptions): FormResult;
 declare function modify(formSchema: FormSchema, options?: ModifyConfig): ModifyResult;
 
-export { type FormResult, type ModifyConfig, type ModifyResult, type ValidationResult, createHeadlessForm, modify };
+export { type FormErrors, type FormResult, type ModifyConfig, type ModifyResult, type ValidationResult, createHeadlessForm, modify };
