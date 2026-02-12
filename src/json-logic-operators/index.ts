bd9d6264eb5b17b071baf0e116c40edaeeb11d51
Daniel Cardoso <daniel.cardoso@remote.com>
2026-02-12 16:15:08 +0000

    Add date_add_months operator

--- src/json-logic-operators/index.ts
+++ src/json-logic-operators/index.ts
@@ -1,7 +1,9 @@
 import { dateAddDays } from './dateAddDays'
+import { dateAddMonths } from './dateAddMonths'
 import { dateDifferenceInMonths } from './dateDifferenceInMonths'
 
 export const operators = {
   date_add_days: dateAddDays,
+  date_add_months: dateAddMonths,
   date_difference_in_months: dateDifferenceInMonths,
 }
